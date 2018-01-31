43cedaed03cec8f3c6ab7bc0821d25b6188ce7bf
Don Lokuge <donlokuge@gmail.com>
2018-01-31 21:26:37 +1300

    node swagger test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Skeleton project for Swagger
+# swagger 2 doc
+# https://swagger.io/docs/specification/2-0/describing-request-body/
---
+++ api/controllers/music.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports.getAllArtists = () => {
+    res.json([{
+            name: "Don",
+            title: "dons"
+        },
+        {
+            name: "Don L",
+            title: "test 123"
+        },
+        {
+            name: "Test",
+            title: "no title"
+        }
+    ]);
+}
+
+module.exports.addArtise = () => {
+
+    
+}
